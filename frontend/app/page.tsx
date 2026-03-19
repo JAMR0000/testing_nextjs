19c2b4a80ef15991737026faadbba26a90ed9fbe
JAMR0000 <albertomarquez0000@gmail.com>
2026-03-19 14:19:22 -0600

    add cache component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,24 @@
-import { getHomePage } from "@/lib/strapi";
-import { HeroSection } from "@/components/hero-section";
+import { getHomePage } from '@/lib/strapi';
+import { HeroSection } from '@/components/hero-section';
+
+export async function generateMetadata() {
+  const strapiData = await getHomePage();
+  const { title, description } = strapiData;
+  return {
+    title,
+    description,
+  };
+}
 
 export default async function Home() {
   const strapiData = await getHomePage();
-  console.log(strapiData.sections);
-  const {title, description} = strapiData;
+  const { title, description } = strapiData;
   const [heroSection] = strapiData?.sections || [];
   return (
-    <main className="container mx-auto py-6">
-      <h1 className="text-3xl font-bold">{title}</h1>
-      <p className="text-lg">{description}</p>
-      <HeroSection data={{...heroSection, title, description}} />
+    <main className='container mx-auto py-6'>
+      <h1 className='text-3xl font-bold'>{title}</h1>
+      <p className='text-lg'>{description}</p>
+      <HeroSection data={heroSection} />
     </main>
   );
 }
